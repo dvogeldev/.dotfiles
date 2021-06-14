69dcbbec86558cb67d0ca13e12d6a5165359a9f8
David Vogel <dvogel@fastmail.com>
2021-06-14 15:25:33 -0400

    Conversion to flakes

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "My config";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.05";
+    home-manager.url = "github:nix-community/home-manager/release-21.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };
+    };
+
+    lib = nixpkgs.lib;
+
+   in {
+     nixosConfigurations = {
+       dv-tp = lib.nixosSystem {
+         inherit system;
+
+	 modules = [
+	   ./system/configuration.nix
+
+	 ];
+       };
+     };
+  };
+}
