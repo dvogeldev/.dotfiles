2934580871b306e4cb06bf21b56c2debd4187cc5
David Vogel <dvogel@fastmail.com>
2021-06-15 11:51:28 -0400

    Add updated flake and modules

--- flake.nix
+++ flake.nix
@@ -2,33 +2,42 @@
   description = "My config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-21.05";
-    home-manager.url = "github:nix-community/home-manager/release-21.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    hardware.url = "github:NixOS/nixos-hardware/master;
+    home = {
+      url = "github:nix-community/home-manager/release-21.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-unstable = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "unstable";
+    };
+    emacs = {
+      url = "github:nix-community/emacs-overlay/master";
+      inputs.nixpkgs.follows = "unstable";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-
-  let
-    system = "x86_64-linux";
-
-    pkgs = import nixpkgs {
-      inherit system;
-      config = { allowUnfree = true; };
+  outputs = { self, nixpkgs, home, ... }@inputs: {
+    nixosConfigurations.dv-tp = nixpkgs.lib.nixosSystem rec {
+      system = "x86_64-linux";
+      modules = [ ./hosts/dv-tp ];
+      specialArgs = { inherit inputs system; };
     };
 
-    lib = nixpkgs.lib;
-
-   in {
-     nixosConfigurations = {
-       dv-tp = lib.nixosSystem {
-         inherit system;
-
-	 modules = [
-	   ./system/configuration.nix
-
-	 ];
-       };
-     };
+    homeConfigurations.home-linux = home.lib.homeManagerConfiguration rec {
+      configuration = ./users/david/home.nix;
+      system = "x86_64-linux";
+      homeDirectory = "/home/david";
+      username = "david";
+      extraSpecialArgs = {
+        inherit inputs system;
+	super = {
+	  device.type = "laptop";
+	  my.username = username;
+        };
+      };
+    };
   };
 }
